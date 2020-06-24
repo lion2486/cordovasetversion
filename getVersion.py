b4993f2197d6f4c600e5c1c5352bb914dc296247
lion2486 <lion_2486@yahoo.gr>
2020-06-24 18:25:32 +0300

    Update getVersion.py

--- getVersion.py
+++ getVersion.py
@@ -3,12 +3,12 @@ import sys
 from xml.dom import minidom
 
 def getVersion(filename):
-  if (sys.path.isfile(filename)):
-    return minidom.parse(filename).getElementsByTagName('widget')[0].getAttribute('version')
-  else
-    print '*****ERROR: config.xml file not exists'
-	  sys.exit(1)
+	if (os.path.isfile(filename)):
+		return minidom.parse(filename).getElementsByTagName('widget')[0].getAttribute('version')
+	else:
+		print '*****ERROR: config.xml file not exists'
+		sys.exit(1)
   
 if __name__ == "__main__":
-    # execute only if run as a script
-    print getVersion(sys.argv[1])
+	# execute only if run as a script
+	print getVersion(sys.argv[1])
