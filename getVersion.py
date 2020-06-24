994f2f3e164c4355e9cfa80f25208443218c919c
lion2486 <lion_2486@yahoo.gr>
2020-06-24 17:54:58 +0300

    parameterizing getVersion file in order to be able to import it.

--- getVersion.py
+++ getVersion.py
@@ -2,5 +2,13 @@ import os
 import sys
 from xml.dom import minidom
 
-if (os.path.isfile(sys.argv[1])):
-  print minidom.parse(sys.argv[1]).getElementsByTagName('widget')[0].getAttribute('version')
+def getVersion(filename):
+  if (sys.path.isfile(filename)):
+    return minidom.parse(filename).getElementsByTagName('widget')[0].getAttribute('version')
+  else
+    print '*****ERROR: config.xml file not exists'
+	  sys.exit(1)
+  
+if __name__ == "__main__":
+    # execute only if run as a script
+    print getVersion(sys.argv[1])
